b9064ae3b39f80d2d987e473a478059dc9cf6277
Tony Zhang <z_haijun@foxmail.com>
2020-01-12 11:05:54 +0800

    display ship num  using ship image

--- score_board.py
+++ score_board.py
@@ -7,10 +7,10 @@ class ScoreBoard:
         self.screen_rect = screen.get_rect()
         self.font = pygame.font.SysFont(None, 48)
         self.font_color = (0, 255, 0)
-        self.ship_num_image = self.font.render(str(settings.ship_total_num), True, self.font_color)
+
+        self.ship_num = status.ship_num
+        self.ship_num_image = pygame.image.load('images/ship.bmp')
         self.ship_num_image_rect = self.ship_num_image.get_rect()
-        self.ship_num_image_rect.right = self.screen_rect.right - self.ship_num_image_rect.width
-        self.ship_num_image_rect.top = self.screen_rect.top + 50
 
         self.score_image = self.font.render(str(status.score), True, self.font_color)
         self.score_rect = self.score_image.get_rect()
@@ -23,12 +23,14 @@ class ScoreBoard:
         self.high_score_rect.top = self.screen_rect.top + 5
 
     def updates(self, status):
-        self.ship_num_image = self.font.render(str(status.ship_num), True, self.font_color)
+        self.ship_num = status.ship_num
         self.score_image = self.font.render(str(status.score), True, self.font_color)
         self.score_rect.right = self.screen_rect.right - self.score_image.get_rect().width
         self.high_score_image = self.font.render(str(status.high_score), True, self.font_color)
 
     def draw(self):
-        self.screen.blit(self.ship_num_image, self.ship_num_image_rect)
+        for num in range(self.ship_num):
+            self.screen.blit(self.ship_num_image, self.ship_num_image_rect)
+            self.ship_num_image_rect.left = num * self.ship_num_image_rect.width
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
